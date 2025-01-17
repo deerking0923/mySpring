1447db224eef3396443ee9a7ba0b2fb1ebdd85a7
realdeer <i3295h@naver.com>
2025-01-17 11:58:02 +0900

    백엔드 완료

--- src/main/java/com/springboot/ezenbackend/config/CustomServletConfig.java
+++ src/main/java/com/springboot/ezenbackend/config/CustomServletConfig.java
@@ -4,6 +4,7 @@ import java.util.Formatter;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.springboot.ezenbackend.controller.LocalDateFormatter;
@@ -12,5 +13,13 @@ import com.springboot.ezenbackend.controller.LocalDateFormatter;
 public class CustomServletConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addFormatter(new LocalDateFormatter());
+    }
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .maxAge(300)
+                .allowedHeaders("Authorization", "Content-Type", "Chache-Control");
     }
 }
--- src/main/java/com/springboot/ezenbackend/controller/TodoController.java
+++ src/main/java/com/springboot/ezenbackend/controller/TodoController.java
@@ -14,9 +14,11 @@ import lombok.RequiredArgsConstructor;
 import java.util.Map;
 
 import org.hibernate.query.Page;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -44,4 +46,16 @@ public class TodoController {
         return Map.of("tno", tno);
     }
 
+    @PutMapping("/{tno}")
+    public Map<String, String> modify(@PathVariable(name = "tno") Long tno, @RequestBody TodoDto todoDto) {
+        todoDto.setTno(tno);
+        todoService.modify(todoDto);
+        return Map.of("Result", "Success");
+    }
+
+    @DeleteMapping("/{tno}")
+    public Map<String, String> remove(@PathVariable(name = "tno") Long tno) {
+        todoService.remove(tno);
+        return Map.of("Result", "Success");
+    }
 }
