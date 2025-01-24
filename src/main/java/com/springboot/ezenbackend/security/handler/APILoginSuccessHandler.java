e69875d20c7aac96db72da8805be01c8fec4f867
realdeer <i3295h@naver.com>
2025-01-24 10:58:44 +0900

    t시큐리티 작업중. 행들러 추가.

--- src/main/java/com/springboot/ezenbackend/security/handler/APILoginSuccessHandler.java
+++ src/main/java/com/springboot/ezenbackend/security/handler/APILoginSuccessHandler.java
@@ -2,23 +2,47 @@ package com.springboot.ezenbackend.security.handler;
 
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import com.google.gson.Gson;
+import com.springboot.ezenbackend.dto.MemberDTO;
+
 import jakarta.servlet.ServletException;
-// import jakarta.servlet.http.HttpServletRequest;
-// import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
 
-// import java.io.IOException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
 
-// import org.springframework.security.core.Authentication;
+import org.springframework.security.core.Authentication;
 
-// public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
+@Log4j2
+public class APILoginSuccessHandler implements AuthenticationSuccessHandler {
 
-// @Override
-// public void onAuthenticationSuccess(HttpServletRequest request,
-// HttpServletResponse response,
-// Authentication authentication) throws IOException, ServletException {
-// // TODO Auto-generated method stub
-// throw new UnsupportedOperationException("Unimplemented method
-// 'onAuthenticationSuccess'");
-// }
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request,
+            HttpServletResponse response,
+            Authentication authentication) throws IOException, ServletException {
+        log.info("----------------");
+        log.info(authentication);
+        log.info("----------------");
 
-// }
+        MemberDTO memberDTO = (MemberDTO) authentication.getPrincipal();
+
+        Map<String, Object> claims = memberDTO.getClaims();
+
+        claims.put("accessToken", "");
+        claims.put("refreshToken", "");
+        Gson gson = new Gson();
+
+        String jsonStr = gson.toJson(claims);
+
+        response.setContentType("application/json");
+
+        PrintWriter printWriter = response.getWriter();
+        printWriter.println(jsonStr);
+        printWriter.close();
+
+    }
+
+}
